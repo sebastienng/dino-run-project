fd63b9c8d43dbdc6a9d1791fea3b3b8246aeff7f
Sebastien Niang <sebastien.niang@gmail.com>
2022-08-08 11:56:31 +0200

    refactor moving dino into classes and creation of first obstacle

--- js/index.js
+++ js/index.js
@@ -1,90 +1,131 @@
-//this function 
-function gameInit(){
-    const canvas = document.querySelector("#canvas-player1");
-    ctx = canvas.getContext("2d");
-    ctx.webkitImageSmoothingEnabled = false;
-    ctx.ImageSmoothingEnabled = false;
 
-    // canvas.height=innerHeight;
-    // canvas.width=innerWidth;
+   
+class Player {
 
-    const background = new Image();
-    background.src = "../images/temporary-background.jpg";
-    background.onload = loadImages;
-    // background.onload = () =>{
-    //     ctx.drawImage(background,0,0,700,250);
-    // }
-    
-    const dinosaur = new Image();
-    dinosaur.src ="../images/dinoCharactersVersion1.1/sheets/DinoSprites - doux.png"
-    dinosaur.onload = loadImages; 
-    // dinosaur.onload =() =>{
-    //     ctx.drawImage(dinosaur,0,0,10 ,30,25,25,10,10);
-    // }
+    constructor(ctx,cvs){
+        this.lives=3;
+        this.posX=0;
+        this.posY=140;
+        this.velocityY=0;
+        this.velocityX=0;
+        this.isJumping=false;
+        this.dinoImage = "../images/dinoCharactersVersion1.1/sheets/DinoSprites - doux.png";
+        this.spriteSize = 24;
+        this.aniframes = 6;
+        this.currentFrame = 0;
+        this.srcX =0;
+        this.startFrame = 4;
+        this.framesDrawn =0;
+        this.initCanvasImage ='';
+        this.context = ctx;
+        this.canvas=cvs;
+        this.init();
+    }
 
-    let col = 24;
-    let spriteWidth = dinosaur.width/col;
-    let spriteHeight = dinosaur.height;
+    init() {
+        const dinosaur = new Image();
+        dinosaur.src = this.dinoImage;
+        this.initCanvasImage = dinosaur;
+    }
 
-    let frames = 6;
-    let currentFrame = 0;
+    setDinoImage(path){
+        this.dinoImage=path;
+    }
 
-    let srcX =0;
-    let srcY = 24;
-
-    let startFrame = 4;
-
-    let framesDrawn =0;
-    let frameSpeed =5;
-    let floorY=140;
-    let posX=0;
-    let velocityY=0;
-    let velocityX=0;
-
-    function animate(){
-        ctx.clearRect(0,0,canvas.width,canvas.height);
-       // requestAnimationFrame(animate);
-        ctx.drawImage(background,0,0,700,250);
-
-        currentFrame = currentFrame % frames;
-        floorY -= velocityY;
-        srcX = spriteWidth*(currentFrame+startFrame)
-        
-        if(floorY<=50) velocityY=-4;
-        if(floorY>=140){
-           velocityY=0;
-        }
-        
-        posX+=velocityX
-        
-       
+    animate(){
+        //  this.context.drawImage(this.background,0,0,700,250);
+     
+       if(this.initCanvasImage) {
+           let spriteWidth = this.initCanvasImage.width/24;
+           let spriteHeight = this.initCanvasImage.height;
+   
+           this.currentFrame = this.currentFrame % this.aniframes;
+           this.posY -= this.velocityY;
+           this.srcX = spriteWidth*(this.currentFrame+this.startFrame)
+           
+           if(this.posY<=50) this.velocityY=-4;
+           if(this.posY>140){
+           this.velocityY=0;
+           this.velocityX=0
+           }
+           
+           this.posX+=this.velocityX
+           
+   
        // ctx.drawImage(dinosaur,98,0,spriteWidth,spriteHeight,150,0,spriteWidth*2,spriteHeight*2);
-        ctx.drawImage(dinosaur,srcX,0,spriteWidth,spriteHeight,posX,floorY,spriteWidth*2,spriteHeight*2);
-        framesDrawn++;
+           this.context.drawImage(this.initCanvasImage,this.srcX,0,spriteWidth,spriteHeight,this.posX,this.posY,spriteWidth*2,spriteHeight*2);
+           this.framesDrawn++;
+   
+           if(this.framesDrawn>=this.frameSpeed){
+               this.currentFrame++;
+               this.framesDrawn=0;
+           }
+           
+       
 
-        if(framesDrawn>=frameSpeed){
-            currentFrame++;
-            framesDrawn=0;
+       }
+     }
+}
+
+
+class Game{
+    constructor(){
+        this.context = '';
+        this.isStarted = false;
+        this.canvas = '';
+        this.background='';
+        this.gameInit();
+        this.playerOne = new Player(this.context,this.canvas);
+        this.element1 = new GameElement(this.context,this.canvas)
+        
+       // this.playerTwo = new Player();
+    }
+//this function 
+    gameInit(){
+        const can = document.querySelector("#canvas-player1");
+        const ctx = can.getContext("2d");
+
+        this.canvas=can;
+        this.context=ctx;
+        
+        ctx.ImageSmoothingEnabled = false;
+   
+        const background = new Image();
+        background.src = "../images/temporary-background.jpg";
+
+
+        this.background=background;
+        background.onload = () =>{
+        this.keyboardListner(this.playerOne)
+        this.update();
         }
+        
+        
 
+    }
+    keyboardListner(player){
         document.addEventListener('keydown', (event)=>{
             switch(event.key){
                 case 'ArrowUp':
-                    startFrame=4;
-                    frames = 6;
-                    velocityY=4;
-                    frameSpeed=5;
+                    player.startFrame=4;
+                    player.aniframes = 6;
+                    player.velocityY=4;
+                    player.frameSpeed=5;
+                    console.log(event.key);
                 break;
                 case 'ArrowDown':
-                    frames = 6;
-                    startFrame=17;
-                    frameSpeed=3;
+                    player.aniframes = 6;
+                    player.startFrame=17;
+                    player.frameSpeed=3;
+                    console.log(event.key);
                 break;
                 case 'ArrowLeft':
-                   velocityX=-3
+                player.velocityX=-3
+                console.log(event.key);
                 break;
                 case 'ArrowRight':
-                   velocityX=3; 
+                player.velocityX=3; 
+                console.log(event.key);
                 break;
                 
                 
@@ -93,30 +134,76 @@ function gameInit(){
         document.addEventListener('keyup', (event)=>{
             switch(event.key){
                 case 'ArrowDown':
-                    startFrame=4;
-                    frames = 6;
-                    frameSpeed=5;
+                    player.startFrame=4;
+                    player.aniframes  = 6;
+                    player.frameSpeed=5;
+                    console.log(event.key);
                 break;
                 case 'ArrowLeft':
-                    velocityX=0
-                 break;
-                 case 'ArrowRight':
-                    velocityX=0
-                 break;
-                 
+                   
+                    // while(player.posY<140){
+                    //     player.velocityX-=0,7
+                    // }
+                    player.velocityX=0
+                    console.log(event.key);
+                break;
+                case 'ArrowRight':
+                    // while(player.posY<140){
+                    //     player.velocityX+=0,7
+                    // }
+                    player.velocityX=0
+                    
+                    console.log(event.key);
+                break;
+                
                 
             }
         })
-      
     }
-    
-   
-    let numOfImage=2;
-    function loadImages(){
-        if(--numOfImage > 0) return;
 
-        animate();  
+    update(){
+        this.context.clearRect(0,0,this.canvas.width,this.canvas.height);
+        this.context.drawImage(this.background,0,0,700,250);
+        this.playerOne.animate();
+        this.element1.drawPosition(); 
+        requestAnimationFrame(() => this.update());
     }
+
+
 }
-   
-gameInit();
+
+class GameElement {
+    constructor(ctx,canvas){
+        this.posY= 145;
+        this.velocityY = 0;
+        this.velocityX = -2;
+        this.aniframes = 6;
+        this.currentFrame = 0;
+        this.srcX = 0;
+        this.startFrame = 4;
+        this.framesDrawn = 0;
+        this.initCanvasImage = '';
+        this.context = ctx;
+        this.canvas = canvas;
+        this.posX=this.canvas.width;
+
+    }
+
+    drawPosition(){
+        this.context.fillStyle = 'red';
+        this.context.fillRect(this.posX, this.posY, 25, 25);
+        
+        // this.framesDrawn++;
+        this.posX+=this.velocityX
+        if(this.posX<-25) this.posX=this.canvas.width;
+        // if(this.framesDrawn>=this.frameSpeed){
+        //     this.currentFrame++;
+        //     this.framesDrawn=0;
+        // }
+    }
+
+    
+}
+
+const game = new Game();
+
