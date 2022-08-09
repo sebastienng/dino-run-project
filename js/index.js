05b27cde1262909d6b7ff975e89f71d0f122c919
Sebastien Niang <sebastien.niang@gmail.com>
2022-08-09 12:37:35 +0200

    fixed bug colision and adding sprite for ennemies

--- js/index.js
+++ js/index.js
@@ -1,100 +1,123 @@
 
-   
+
 class Player {
 
-    constructor(ctx,cvs){
-        this.lives=3;
-        this.score=0;
+    constructor(ctx, cvs) {
+        this.lives = 3;
+        this.score = 0;
         this.scoreLayout = document.querySelector('.display-player-info>span');
-        this.posX=0;
-        this.posY=140;
-        this.velocityY=0;
-        this.velocityX=0;
-        this.isJumping=false;
+        this.posX = 0;
+        this.posY = 140;
+        this.velocityY = 0;
+        this.velocityX = 0;
+        this.isJumping = false;
         this.dinoImage = "../images/dinoCharactersVersion1.1/sheets/DinoSprites - doux.png";
-        this.shadowImage= "../images/dinoCharactersVersion1.1/misc/shadow_2.png"
+        this.shadowImage = "../images/dinoCharactersVersion1.1/misc/shadow_2.png"
         this.spriteSize = 24;
         this.aniframes = 6;
         this.currentFrame = 0;
-        this.srcX =0;
+        this.srcX = 0;
         this.startFrame = 4;
-        this.framesDrawn =0;
-        this.initCanvasImage ='';
+        this.framesDrawn = 0;
+        this.initCanvasImage = '';
         this.shadowImageCanvas = ''
         this.context = ctx;
-        this.canvas=cvs;
+        this.canvas = cvs;
         this.init();
-        this.spriteWidth = this.initCanvasImage.width/24;
+        this.spriteWidth = this.initCanvasImage.width / 24;
         this.spriteHeight = this.initCanvasImage.height;
-        this.isTouched=false
-        this.frameSpeed=5;
-    
+        this.isTouched = false
+        this.frameSpeed = 5;
+
 
     }
 
     init() {
+
         const dinosaur = new Image();
         const shadow = new Image();
         dinosaur.src = this.dinoImage;
-       
-        shadow.src= this.shadowImage;
+
+        shadow.src = this.shadowImage;
         this.initCanvasImage = dinosaur;
-        this.shadowImageCanvas= shadow;
-       // this.context.drawImage(this.initCanvasImage,0,0,this.spriteWidth,this.spriteHeight,0,0,this.spriteWidth*2,this.spriteHeight*2);
+        this.shadowImageCanvas = shadow;
+        this.initLives();
+        // this.context.drawImage(this.initCanvasImage,0,0,this.spriteWidth,this.spriteHeight,0,0,this.spriteWidth*2,this.spriteHeight*2);
+    }
+    initLives() {
+        const livesDiv = document.querySelector("#lives-display")
+        for (let i = 0; i < this.lives; i++) {
+            const newDiv = document.createElement('div')
+            newDiv.classList.add('lives')
+            livesDiv.appendChild(newDiv)
+        }
     }
 
-    basicRun(){
-        this.startFrame=4;
+    addLife() {
+        const livesDiv = document.querySelector("#lives-display")
+        const newDiv = document.createElement('div')
+        newDiv.classList.add('lives')
+        livesDiv.appendChild(newDiv)
+    }
+    removeLife() {
+        const livesDiv = document.querySelector("#lives-display")
+        const life = document.querySelector('.lives')
+        console.log(life);
+        livesDiv.removeChild(life)
+    }
+
+    basicRun() {
+        this.startFrame = 4;
         this.aniframes = 6;
-        this.frameSpeed=5;
+        this.frameSpeed = 5;
     }
 
-    getHit(){
+    getHit() {
         this.startFrame = 13;
         this.aniframes = 4;
         this.currentFrame = 0;
-        this.framesDrawn =0;
+        this.framesDrawn = 0;
     }
 
-    setDinoImage(path){
-        this.dinoImage=path;
+    setDinoImage(path) {
+        this.dinoImage = path;
     }
 
-    animate(){
+    animate() {
         //  this.context.drawImage(this.background,0,0,700,250);
-     
-       if(this.initCanvasImage) {
-   
-           this.currentFrame = this.currentFrame % this.aniframes;
-           this.posY -= this.velocityY;
-           this.srcX = this.spriteWidth*(this.currentFrame+this.startFrame)
-           
-           if(this.posY<=50) this.velocityY=-4;
-           if(this.posY>140){
-            this.velocityY=0;
-            this.posY=140;
-            this.isJumping=false;
-           }
-           
-           this.posX+=this.velocityX
-           
-   
-       // ctx.drawImage(dinosaur,98,0,spriteWidth,spriteHeight,150,0,spriteWidth*2,spriteHeight*2);
-           this.context.drawImage(this.initCanvasImage,this.srcX,0,this.spriteWidth,this.spriteHeight,this.posX,this.posY,this.spriteWidth*2,this.spriteHeight*2);
-         
-           this.framesDrawn++;
-   
-           if(this.framesDrawn>=this.frameSpeed){
-               this.currentFrame++;
-               
-               this.score++;
-               this.scoreLayout.textContent=this.score;
-               this.framesDrawn=0;
-           }
-           //this.setScore()
 
-       }
-     }
+        if (this.initCanvasImage) {
+
+            this.currentFrame = this.currentFrame % this.aniframes;
+            this.posY -= this.velocityY;
+            this.srcX = this.spriteWidth * (this.currentFrame + this.startFrame)
+
+            if (this.posY <= 50) this.velocityY = -4;
+            if (this.posY > 140) {
+                this.velocityY = 0;
+                this.posY = 140;
+                this.isJumping = false;
+            }
+
+            this.posX += this.velocityX
+
+
+            // ctx.drawImage(dinosaur,98,0,spriteWidth,spriteHeight,150,0,spriteWidth*2,spriteHeight*2);
+            this.context.drawImage(this.initCanvasImage, this.srcX, 0, this.spriteWidth, this.spriteHeight, this.posX, this.posY, this.spriteWidth * 2, this.spriteHeight * 2);
+
+            this.framesDrawn++;
+
+            if (this.framesDrawn >= this.frameSpeed) {
+                this.currentFrame++;
+
+                this.score++;
+                this.scoreLayout.textContent = this.score;
+                this.framesDrawn = 0;
+            }
+            //this.setScore()
+
+        }
+    }
 
     //  setScore(){
     //     if(this.score<10){
@@ -106,149 +129,179 @@ class Player {
     //     }
     //     console.log(this.score);
     //  }
-     moveUp(){
-        this.velocityY=4;
-        this.isJumping=true;
-     }
+    moveUp() {
+        this.velocityY = 5;
+        this.isJumping = true;
+    }
 
-     moveRight(){
-        this.velocityX=3; 
-     }
+    moveRight() {
+        this.velocityX = 3;
+    }
 
-     moveLeft(){
-        this.velocityX=-3
-     }
+    moveLeft() {
+        this.velocityX = -3
+    }
 
-     moveDown(){
+    moveDown() {
         this.aniframes = 6;
-        this.startFrame=17;
-        this.frameSpeed=3;
-     }
+        this.startFrame = 17;
+        this.frameSpeed = 3;
+    }
 
-     left() {
+    left() {
         return this.posX;
-      }
-      right() {
-        return this.posX + this.spriteWidth;
-      }
-      top() {
+    }
+    right() {
+        return this.posX + this.spriteWidth * 2;
+    }
+    top() {
         return this.posY;
-      }
-      bottom() {
-        return this.posY + this.spriteHeight;
-      }
-     
-      crashWith(obstacle) {
-        return !(this.bottom() < obstacle.top() || this.top() > obstacle.bottom() || this.right() < obstacle.left() || this.left() > obstacle.right());
-      }
+    }
+    bottom() {
+        return this.posY + this.spriteHeight * 2;
+    }
+
+    crashWith(obstacle) {
+        console.log("checking collisions " + this.top(), obstacle.bottom())
+        //  return !(this.bottom() < obstacle.top() || this.top() > obstacle.bottom() || this.right() < obstacle.left() || this.left() > obstacle.right());
+
+        if (
+            this.right() > obstacle.left() && this.right() < obstacle.right()
+            ||
+            this.left() > obstacle.left() && this.left() < obstacle.right()
+        ) {
+            if (
+                this.top() > obstacle.top() && this.top() < obstacle.bottom()
+                ||
+                this.bottom() > obstacle.top() && this.bottom() < obstacle.bottom()
+            ) {
+                return true;
+            }
+        }
+        return false
+    }
 }
 
 
-class Game{
-    constructor(){
+class Game {
+    constructor() {
         this.context = '';
         this.isStarted = false;
         this.canvas = '';
         this.background = '';
+        this.enn = 0;
+        this.maxEnnemie = 1;
         this.gameInit();
-        this.playerOne = new Player(this.context,this.canvas);
-        this.element1 = new GameElement(25,35,this.context,this.canvas)
-        
-       // this.playerTwo = new Player();
+        this.playerOne = new Player(this.context, this.canvas);
+        this.element1 = new Ennemy('../images/ennemies sprites/Dragonfly Sprite Sheet.png', 7, 4, 4, this.context, this.canvas)
+        this.arrayEnnemies = [
+            new Ennemy('../images/ennemies sprites/Akaname Sprite Sheet.png', 8, 4, 5, this.context, this.canvas), 
+            new Ennemy('../images/ennemies sprites/Brain Mole Monarch Sprite Sheet.png', 7, 4, 4, this.context, this.canvas), 
+            new Ennemy('../images/ennemies sprites/Dragonfly Sprite Sheet.png', 7, 4, 4, this.context, this.canvas),
+            new Ennemy('../images/ennemies sprites/Intellect Devourer Sprites.png', 8, 6, 4, this.context, this.canvas),
+            new Ennemy('../images/ennemies sprites/Jellyfish Sprite Sheet.png', 7, 5, 5, this.context, this.canvas),
+            new Ennemy('../images/ennemies sprites/Porcupine Sprite Sheet.png', 5, 5, 5, this.context, this.canvas),
+            new Ennemy('../images/ennemies sprites/Shardsoul Slayer Sprite Sheet.png', 8, 5, 8, this.context, this.canvas)];
+        // this.playerTwo = new Player();
     }
-//this function 
-    gameInit(){
+    //this function 
+    gameInit() {
         const can = document.querySelector("#canvas-player1");
-        const ctx = can.getContext("2d");
 
-        this.canvas=can;
-        this.context=ctx;
-        
-        ctx.ImageSmoothingEnabled = false;
-   
+        const ctx = can.getContext("2d");
+        ctx.imageSmoothingEnabled = false;
+        this.canvas = can;
+        this.context = ctx;
+
+
         const background = new Image();
         background.src = "../images/temporary-background.jpg";
 
 
-        this.background=background;
-        background.onload = () =>{
-        this.keyboardListner(this.playerOne)
-        this.update();
+        this.background = background;
+        background.onload = () => {
+            this.keyboardListner(this.playerOne)
+            this.update();
         }
-        
-        
+
+
 
     }
-    keyboardListner(player){
-        document.addEventListener('keydown', (event)=>{
-            switch(event.key){
+    keyboardListner(player) {
+        document.addEventListener('keydown', (event) => {
+            switch (event.key) {
                 case 'o':
-                    if(!player.isJumping) player.moveUp();
-                break;
+                    if (!player.isJumping) player.moveUp();
+                    break;
                 case 'l':
                     player.moveDown();
-                break;
+                    break;
                 case 'k':
                     player.moveLeft();
-                break;
+                    break;
                 case 'm':
                     player.moveRight();
-                break;
-                
-                
+                    break;
+
+
             }
-          //  console.log(event.key);
+            //  console.log(event.key);
         })
-        document.addEventListener('keyup', (event)=>{
-            switch(event.key){
+        document.addEventListener('keyup', (event) => {
+            switch (event.key) {
                 case 'l':
-                    player.startFrame=4;
-                    player.aniframes  = 6;
-                    player.frameSpeed=5;
-                break;
+                    player.startFrame = 4;
+                    player.aniframes = 6;
+                    player.frameSpeed = 5;
+                    break;
                 case 'k':
-                    player.velocityX=0
-                break;
+                    player.velocityX = 0
+                    break;
                 case 'm':
-                    player.velocityX=0
-                    
-                break;
-                
-                
+                    player.velocityX = 0
+
+                    break;
+
+
             }
         })
     }
 
-    update(){
+    update() {
+       
+        if (this.playerOne.lives > 0) {
+            if (this.playerOne.crashWith(this.element1) && !this.playerOne.isTouched) {
 
-        if(this.playerOne.lives>0){
-            if(this.playerOne.crashWith(this.element1) && !this.playerOne.isTouched){
-                console.log("touché");
                 this.playerOne.getHit()
                 setTimeout(() => {
                     this.playerOne.lives--;
-                    this.playerOne.isTouched=false;
+                    this.playerOne.isTouched = false;
+
                     this.playerOne.basicRun();
-                },800)
-                 this.playerOne.isTouched=true;
-               
+                }, 800)
+                this.playerOne.isTouched = true;
+                this.playerOne.removeLife();
+
             }
-            
-            if(this.playerOne.score%100===0){
-                // console.log(`element fast went from ${this.element1.velocityX}`);
-                this.element1.velocityX-=0.2;
-                console.log("multiple de 100");
-                // console.log(`to ${this.element1.velocityX} ` );
+
+            if (this.playerOne.score % 100 === 0) {
+                this.element1.velocityX -= 0.2;
             }
-            this.context.clearRect(0,0,this.canvas.width,this.canvas.height);
-            this.context.drawImage(this.background,0,0,700,250);
+            this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
+            this.context.drawImage(this.background, 0, 0, 700, 250);
             this.playerOne.animate();
-            this.element1.drawPosition(); 
+            this.arrayEnnemies[enn].drawPosition();
+            
+            if(en<this.arrayEnnemies.length) {
+                this.enn++;
+            }else{
+                this.enn=0;
+            }
             requestAnimationFrame(() => this.update());
-        }else{
+        } else {
             console.log("lost");
         }
-       
+
 
 
 
@@ -256,52 +309,142 @@ class Game{
 }
 
 class GameElement {
-    constructor(height, width,ctx,canvas){
-        this.height=height;
-        this.width=width
-        this.posY= 145;
+    constructor(ctx, canvas) {
+        this.height = 25;
+        this.width = 35
+        this.posY = 145;
         this.velocityY = 0;
         this.velocityX = -1.8;
         this.aniframes = 6;
         this.currentFrame = 0;
         this.srcX = 0;
+        this.srcY = 0;
         this.startFrame = 4;
         this.framesDrawn = 0;
         this.initCanvasImage = '';
         this.context = ctx;
         this.canvas = canvas;
-        this.posX=this.canvas.width;
+        this.posX = this.canvas.width;
+
+        this.context = ctx;
+        this.canvas = canvas;
+        this.spriteWidth = '';
+        this.spriteHeight = '';
+        this.frameSpeed = 5;
 
     }
 
-    isOutofCanvas(){
-        if(this.posX<0)
-        return true
+    isOutofCanvas() {
+        if (this.posX < 0)
+            return true
     }
-    drawPosition(){
+    drawPosition() {
         this.context.fillStyle = 'red';
         this.context.fillRect(this.posX, this.posY, this.height, this.width);
-        this.posX+=this.velocityX
-        if(this.posX<-25) this.posX=this.canvas.width;
-    
+        this.posX += this.velocityX
+        if (this.posX < -25) this.posX = this.canvas.width;
+
     }
 
     left() {
         return this.posX;
-      }
-      right() {
+    }
+    right() {
         return this.posX + this.width;
-      }
-      top() {
+    }
+    top() {
         return this.posY;
-      }
-      bottom() {
+    }
+    bottom() {
         return this.posY + this.height;
-      }
-     
-      crashWith(obstacle) {
-        return !(this.bottom() < obstacle.top() || this.top() > obstacle.bottom() || this.right() < obstacle.left() || this.left() > obstacle.right());
-      }
+    }
+
+    crashWith(obstacle) {
+        return !(this.bottom() < obstacle.top() || this.top() < obstacle.bottom() || this.right() < obstacle.left() || this.left() > obstacle.right());
+    }
+}
+
+class Ennemy extends GameElement {
+
+    constructor(src, col, row, frames, ctx, canv) {
+        super(ctx, canv);
+        this.ennemiSrc = src;
+        this.init()
+        this.posY = 50;
+        this.startFrame = 0;
+        this.spriteHeight = this.initCanvasImage.height / row;
+        this.spriteWidth = this.initCanvasImage.width / col;
+        this.aniframes = frames;
+
+    }
+
+    init() {
+        const myImage = new Image();
+
+        myImage.src = this.ennemiSrc;
+
+        this.initCanvasImage = myImage;
+
+        // this.context.drawImage(this.initCanvasImage,0,0,this.spriteWidth,this.spriteHeight,0,0,this.spriteWidth*2,this.spriteHeight*2);
+    }
+
+    left() {
+        return this.posX;
+    }
+    right() {
+        return this.posX + this.spriteWidth * 2;
+    }
+    top() {
+        return this.posY;
+    }
+    bottom() {
+        return this.posY + this.spriteHeight * 2;
+    }
+
+
+    drawPosition() {
+        if (this.initCanvasImage) {
+
+            this.currentFrame = this.currentFrame % this.aniframes;
+            this.srcX = this.spriteWidth * (this.currentFrame + this.startFrame)
+
+
+            this.posX += this.velocityX
+
+
+            // Save the current context  
+            this.context.save();
+            // Perform the "flip" horizontal  
+            this.context.scale(-1, 1);
+            // Finally we draw the image
+            this.context.drawImage(
+                this.initCanvasImage,
+                this.srcX,
+                0,
+                this.spriteWidth,
+                this.spriteHeight,
+                // flipping x-coordinates
+                -this.posX,
+                this.posY,
+                this.spriteWidth * 2,
+                this.spriteHeight * 2
+            )
+            // And restore the context ready for the next loop  
+            this.context.restore();
+
+            this.framesDrawn++;
+
+            if (this.framesDrawn >= this.frameSpeed) {
+
+                this.currentFrame++;
+                this.framesDrawn = 0;
+            }
+            //this.setScore()
+            if (this.posX < -25) this.posX = this.canvas.width;
+        }
+    }
+
+
 }
 
 //const game = new Game();
@@ -320,15 +463,20 @@ class GameElement {
 const startBtn = document.querySelector('#start');
 const settingsBtn = document.querySelector('#settings');
 const leaderboardBtn = document.querySelector('#leaderboard');
+const replayBtn = document.querySelector('#replay');
+let game = '';
 
-startBtn.addEventListener('click', ()=>{
-    console.log("Need to hide stuff and start the game");
+startBtn.addEventListener('click', () => {
+
     const allHidenElements = document.querySelectorAll(".hidden-elements")
     startBtn.classList.toggle("hidden-elements")
     settingsBtn.classList.toggle("hidden-elements")
     leaderboardBtn.classList.toggle("hidden-elements")
-    allHidenElements.forEach((element)=>{
+    allHidenElements.forEach((element) => {
         element.classList.toggle("hidden-elements")
     })
-    const game = new Game();
+    game = new Game();
+})
+replayBtn.addEventListener('click', () => {
+    game = new Game();
 })
